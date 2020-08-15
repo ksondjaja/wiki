4172e52eda6bc9c943a7bca7f8bb55de0187e339
ksondjaja <sondjajaja@gmail.com>
2020-08-15 14:50:54 -0400

    Edited settings to create static file

--- wiki/settings.py
+++ wiki/settings.py
@@ -118,4 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_TMP = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+
+os.makedirs(STATIC_TMP, exist_ok=True)
